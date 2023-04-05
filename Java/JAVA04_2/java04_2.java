6fe2f7339d8c36f40ee4f7e8581531595427898a
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-04-05 17:18:42 +0200

    randompush

--- Java/JAVA04_2/java04_2.java
+++ Java/JAVA04_2/java04_2.java
@@ -16,22 +16,6 @@ public class java04_2 {
                 break;
             default:
                 System.out.println("Grand");
-
         }
     }
-
-    // public static void java42(String[] args) {
-    // for (int i = 0; i < args.length; i++) {
-    // int variable = Integer.parseInt(args[i]);
-    // if (variable <= 1000) {
-    // System.out.println("0%");
-    // } else if (variable >= 1000 && variable <= 2000) {
-    // System.out.println("1%");
-    // } else if (variable >= 2000 && variable <= 5000) {
-    // System.out.println("3%");
-    // } else {
-    // System.out.println("5%");
-    // }
-    // }
-    // }
 }
