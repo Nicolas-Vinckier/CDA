fa919cccfe0da8300f19143aa6f90620edfec885
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-04-05 16:08:58 +0200

    Update java04.java

--- Java/JAVA04/java04.java
+++ Java/JAVA04/java04.java
@@ -1,5 +1,17 @@
 package Java.JAVA04;
 
 public class java04 {
-    
+    public static void main(String[] args) {
+        int variable = 500;
+
+        if (variable < 1000) {
+            System.out.println("0%");
+        } else if (nombre > 1000 && nombre < 2000) {
+            System.out.println("1%");
+        } else if (nombre > 2000 && nombre < 5000) {
+            System.out.println("3%");
+        } else {
+            System.out.println("5%");
+        }
+    }
 }
