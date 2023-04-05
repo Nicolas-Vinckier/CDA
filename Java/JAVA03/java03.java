1fde3ad466df14cab02d5cc0ccd82cbfb41707b1
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-04-05 15:11:03 +0200

    Update java03.java

--- Java/JAVA03/java03.java
+++ Java/JAVA03/java03.java
@@ -1,5 +1,9 @@
 package Java.JAVA03;
 
 public class java03 {
-    
+    public static void main(String[] args) {
+        byte x = 10; 
+        x += 1000; 
+        System.out.println(x);
+    }
 }
