62f95bd09908f30c031a6115e5051088167a8bb3
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-04-05 16:47:50 +0200

    Update java04_2.java

--- Java/JAVA04_2/java04_2.java
+++ Java/JAVA04_2/java04_2.java
@@ -1,5 +1,23 @@
 package Java.JAVA04_2;
 
 public class java04_2 {
-    
+    public static void main(String[] args) {
+        int poid = 3;
+
+        switch (poid) {
+            case 0:
+                System.out.println("petit");
+                break;
+            case 1:
+                System.out.println("petit");
+                break;
+            case 2:
+                System.out.println("Moyen");
+                break;
+            default:
+                System.out.println("Grand");
+
+        }
+
+    }
 }
