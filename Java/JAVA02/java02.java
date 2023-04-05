9c404b39e0055177c63463d742c3ca990c6965b8
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-04-05 14:15:17 +0200

    java02

---
+++ Java/JAVA02/java02.java
@@ -0,0 +1,15 @@
+package Java.JAVA02;
+
+public class java02 {
+    public static void main(String[] args) {
+        // int variable1 = 1234;
+        // float variable2 = variable1;
+        // System.out.println("Variable 1 : " + variable1);
+        // System.out.println("Variable 2 : " + variable2);
+        // int variable3 = 123456789;
+        // float variable4 = variable3;
+        // System.out.println("Variable 3 : " + variable3);
+        // System.out.println("Variable 4 : " + variable4);
+        
+    }
+}
