f3b01cc06007fd212ad05400d02d6e7168d8e31f
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-04-05 17:03:56 +0200

    patch java04_2

--- Java/JAVA04_2/java04_2.java
+++ Java/JAVA04_2/java04_2.java
@@ -2,7 +2,7 @@ package Java.JAVA04_2;
 
 public class java04_2 {
     public static void main(String[] args) {
-        int poid = 3;
+        int poid = Integer.parseInt(args[0]);
 
         switch (poid) {
             case 0:
@@ -18,6 +18,20 @@ public class java04_2 {
                 System.out.println("Grand");
 
         }
-
     }
+
+    // public static void java42(String[] args) {
+    // for (int i = 0; i < args.length; i++) {
+    // int variable = Integer.parseInt(args[i]);
+    // if (variable <= 1000) {
+    // System.out.println("0%");
+    // } else if (variable >= 1000 && variable <= 2000) {
+    // System.out.println("1%");
+    // } else if (variable >= 2000 && variable <= 5000) {
+    // System.out.println("3%");
+    // } else {
+    // System.out.println("5%");
+    // }
+    // }
+    // }
 }
