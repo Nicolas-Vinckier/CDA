9f172f0a09a17fb709ffe62d9eecce39f4d7b871
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-04-05 16:22:52 +0200

    Update java04.java

--- Java/JAVA04/java04.java
+++ Java/JAVA04/java04.java
@@ -2,16 +2,17 @@ package Java.JAVA04;
 
 public class java04 {
     public static void main(String[] args) {
-        int variable = 100;
-
-        if (variable <= 1000) {
-            System.out.println("0%");
-        } else if (variable >= 1000 && variable <= 2000) {
-            System.out.println("1%");
-        } else if (variable >= 2000 && variable <= 5000) {
-            System.out.println("3%");
-        } else {
-            System.out.println("5%");
+        for (int i = 0; i < args.length; i++) {
+            int variable = Integer.parseInt(args[i]);
+            if (variable <= 1000) {
+                System.out.println("0%");
+            } else if (variable >= 1000 && variable <= 2000) {
+                System.out.println("1%");
+            } else if (variable >= 2000 && variable <= 5000) {
+                System.out.println("3%");
+            } else {
+                System.out.println("5%");
+            }
         }
     }
 }
