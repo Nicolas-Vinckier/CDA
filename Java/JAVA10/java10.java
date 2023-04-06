440939a5e5f2272ae55101dd260af67065f238d9
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-04-06 16:11:17 +0200

    Create java10.java

---
+++ Java/JAVA10/java10.java
@@ -0,0 +1,40 @@
+package Java.JAVA10;
+
+public class java10 {
+    public static void main(String[] args) {
+
+        int[] tab = { x };
+
+        for (int i : tab) {
+            if () {
+
+            }
+        }
+        System.out.println();
+
+    }
+}
+
+/*
+ * JAVA10 : Trier un tableau d’entiers
+ * 
+ * Le but de ce TP est de produire un algorithme basé sur les instructions de
+ * bases.
+ * 
+ * L'exercice consiste à trier un tableau de 5 entiers saisis par l’utilisateur.
+ * 
+ * Utiliser le scanner pour demander la saisie de 5 entiers.
+ * 
+ * Appelez 5 fois la méthode clavier.nextInt().
+ * 
+ * Pour chaque saisie, stocker le nombre dans un tableau en le triant par ordre
+ * croissant.
+ * 
+ * Afficher la liste de ces entiers triée.
+ * 
+ * 
+ * $ java TpJava10
+ * Saisissez 5 entiers séparés par un espace : 5 15 2 9 1
+ * Le tableau trié est : 1, 2, 5, 9, 15
+ * 
+ */
