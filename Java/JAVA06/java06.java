ebcb5143330de9ac79b97bee0b996aaf84111365
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-04-06 09:45:57 +0200

    Create java06.java

---
+++ Java/JAVA06/java06.java
@@ -0,0 +1,12 @@
+package Java.JAVA06;
+
+public class java06 {
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[0]);
+        int i = 1; 
+            while (i <= x){
+                System.out.println(i);
+                i++;
+            }
+    }
+}
