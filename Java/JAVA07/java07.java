b394935d03e570f9475382ed28fbd49163a27271
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-04-06 10:25:15 +0200

    Create java07.java

---
+++ Java/JAVA07/java07.java
@@ -0,0 +1,14 @@
+package Java.JAVA07;
+
+public class java07 {
+    public static void main(String[] args) {
+        int number = Integer.parseInt(args[0]);
+        int factorial = 1; 
+
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+
+        System.out.println(factorial);
+    }
+}
