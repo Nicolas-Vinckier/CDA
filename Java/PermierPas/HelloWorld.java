37921092c0e4f770fce1ba4690470cb0c2c2318d
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-04-06 16:04:10 +0200

    java10

--- Java/PermierPas/HelloWorld.java
+++ Java/PermierPas/HelloWorld.java
@@ -1,7 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        for(int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
-         }
+        int i = 0;
+        if (i) {
+            System.out.println(i);
+        }
     }
 }
