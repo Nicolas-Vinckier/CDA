9ff1ac8060618bd29131622a58b2aae8a2b76e05
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-04-06 11:24:01 +0200

    Create saisie.java

---
+++ Java/saisie.java
@@ -0,0 +1,12 @@
+// package Java;
+
+// import java.util.Scanner;
+
+// public class saisie {
+//     public static void main(String[] args) {
+//         Scanner clavier = new Scanner(System.in);
+//         System.out.println("Saisir un entier");
+//         int entier = clavier.nextInt();
+//         System.out.println("L'entier saisie est :" + entier);
+//     }
+// }
